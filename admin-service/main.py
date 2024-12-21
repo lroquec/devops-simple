e665fb32478a1fc913eb6330df6419011f248eb9
Laura Roque <lroquecu@gmail.com>
2024-12-21 18:00:19 +0100

    revert styles

--- admin-service/main.py
+++ admin-service/main.py
@@ -29,6 +29,11 @@ with app.app_context():
     except Exception as e:
         print("Error connecting to the database:", str(e))
 
+@app.before_request
+def clear_session_on_start():
+    if request.endpoint == 'login':
+        session.clear()  # Asegúrate de que no haya residuos de sesiones previas.
+
 @app.route("/")
 def index():
     return redirect(url_for("login"))
