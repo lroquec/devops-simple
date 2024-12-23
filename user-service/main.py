35455b13bac051cef4647f79dcd6c7e9664794ed
Laura Roque <lroquecu@gmail.com>
2024-12-23 12:34:06 +0100

    change mysql pass

--- user-service/main.py
+++ user-service/main.py
@@ -17,7 +17,7 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 # Database connection details (using environment variables for Docker compatibility)
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
 app.config["MYSQL_USER"] = os.getenv("MYSQL_USER", "root")
-app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD", "")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD", "example")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB", "pythonlogin")
 
 # Initialize MySQL
